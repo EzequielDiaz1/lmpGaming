08017025c910d6a4fbf4988d753a7c327ddef16a
EzequielDiaz1 <ezequiel.diaz1337@gmail.com>
2020-10-30 16:37:05 -0300

    prueba

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const imgLogo = {  
+    width: '510px',
+    height: '475px',
+    opacity: '0.9',
+    mixBlendMode: 'difference '
+}
+
+const imgGoLMP = {
+    width: '142px',
+    height: '100px',
+    opacity: '0.9'
+}
+
+
+const Home = () => {
+    return (
+        <div>
+            <div style={{display:'flex', justifyContent: 'center',marginTop:'200px'}}>
+                <a href="https://www.instagram.com/lmpgaming/"><img src="https://i.ibb.co/TBbnyfb/Logo-LMP-Original-PNG.png" style={imgLogo} alt=""/></a>
+            </div>
+            <div style={{display:'flex', justifyContent: 'center',marginTop:'30px'}}>
+                <img src="https://i.ibb.co/7VzPD7Z/Go-LMP-PNG.png" style={imgGoLMP}></img>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
